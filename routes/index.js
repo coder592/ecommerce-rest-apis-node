3239fcffbfc74734a743e140bd40ff48476ca7d4
arvind <dpyadavmnnit@gmail.com>
2021-04-23 12:08:23 +0545

    'routes'

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,11 @@ router.get('/me', auth, userController.me);
 router.post('/refresh', refreshController.refresh);
 router.post('/logout', auth, loginController.logout);
 
-
+router.post('/products', [auth, admin], productController.store);
+router.put('/products/:id', [auth, admin], productController.update);
+router.delete('/products/:id', [auth, admin], productController.destroy);
+router.get('/products', productController.index);
+router.get('/products/:id', productController.show);
 
 
 export default router;
